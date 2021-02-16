9adeb20ea8f8ca3288e4f64b6dc5ee2a5cad0f1f
rqguzman <rafael.guzman@outlook.com>
2021-02-16 16:46:22 -0300

    :sparkles: Feat(Enrollment): Add Enrollment entity

---
+++ backend/src/main/java/com/devsuperior/dslearn/entities/Enrollment.java
@@ -0,0 +1,94 @@
+package com.devsuperior.dslearn.entities;
+
+import com.devsuperior.dslearn.entities.pk.EnrollmentPK;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_enrollment")
+public class Enrollment implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @EmbeddedId
+    private EnrollmentPK id = new EnrollmentPK();
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant enrollMoment;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+
+    private Instant refundMoment;
+
+    private boolean isAvailable;
+
+    private boolean isOnlyUpdate;
+
+    public Enrollment() {
+    }
+
+    public Enrollment(User user, Offer offer, Instant enrollMoment,
+                      Instant refundMoment, boolean isAvailable,
+                      boolean isOnlyUpdate) {
+        id.setUser(user);
+        id.setOffer(offer);
+        this.enrollMoment = enrollMoment;
+        this.refundMoment = refundMoment;
+        this.isAvailable = isAvailable;
+        this.isOnlyUpdate = isOnlyUpdate;
+    }
+
+    public User getStudent() {
+        return id.getUser();
+    }
+
+    public void setStudent(User user) {
+        id.setUser(user);
+    }
+
+    public Offer getOffer() {
+        return id.getOffer();
+    }
+
+    public void setOffer(Offer offer) {
+        id.setOffer(offer);
+    }
+
+    public Instant getEnrollMoment() {
+        return enrollMoment;
+    }
+
+    public void setEnrollMoment(Instant enrollMoment) {
+        this.enrollMoment = enrollMoment;
+    }
+
+    public Instant getRefundMoment() {
+        return refundMoment;
+    }
+
+    public void setRefundMoment(Instant refundMoment) {
+        this.refundMoment = refundMoment;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    public boolean isOnlyUpdate() {
+        return isOnlyUpdate;
+    }
+
+    public void setOnlyUpdate(boolean onlyUpdate) {
+        isOnlyUpdate = onlyUpdate;
+    }
+
+
+}
