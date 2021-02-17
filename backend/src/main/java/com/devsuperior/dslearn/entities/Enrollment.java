bff888a6463fc2ad57e7a47e352c20d8442bec89
rqguzman <rafael.guzman@outlook.com>
2021-02-17 11:31:57 -0300

    :sparkles: Feat(Lesson): Add Lesson and its child entities

--- backend/src/main/java/com/devsuperior/dslearn/entities/Enrollment.java
+++ backend/src/main/java/com/devsuperior/dslearn/entities/Enrollment.java
@@ -2,12 +2,11 @@ package com.devsuperior.dslearn.entities;
 
 import com.devsuperior.dslearn.entities.pk.EnrollmentPK;
 
-import javax.persistence.Column;
-import javax.persistence.EmbeddedId;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_enrollment")
@@ -27,6 +26,9 @@ public class Enrollment implements Serializable {
     private boolean isAvailable;
 
     private boolean isOnlyUpdate;
+
+    @ManyToMany(mappedBy = "enrollmentsDone")
+    private Set<Lesson> lessonsDone = new HashSet<>();
 
     public Enrollment() {
     }
