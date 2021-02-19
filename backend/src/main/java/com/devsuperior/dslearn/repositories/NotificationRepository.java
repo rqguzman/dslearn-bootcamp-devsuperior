9bb13483e957c7ccc41af21da9e87e8cd517b425
rqguzman <rafael.guzman@outlook.com>
2021-02-19 13:33:56 -0300

    :sparkles: Feat(Data Access Layer): Add repositories

---
+++ backend/src/main/java/com/devsuperior/dslearn/repositories/NotificationRepository.java
@@ -0,0 +1,8 @@
+package com.devsuperior.dslearn.repositories;
+
+import com.devsuperior.dslearn.entities.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+}
